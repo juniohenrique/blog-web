0de661ae435a2977949575130fc0516fe961b70f
Fernando Papito <fernando@qaninja.io>
2018-03-05 20:53:25 -0300

    incluindo headless no teste

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -1,7 +1,12 @@
 require 'capybara'
 require 'capybara/cucumber'
+require 'capybara/poltergeist'
 require 'faker'
 require 'site_prism'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Poltergeist::Driver.new(app, options = { js_errors: false })
+end
 
 Capybara.configure do |c|
   c.default_driver = :selenium
